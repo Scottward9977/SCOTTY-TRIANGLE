1846f96a287e20e536f26c23bd2922e989cc4ae8
Nominal9977 <84743651+Nominal9977@users.noreply.github.com>
2024-09-23 14:41:35 -0400

    got it working with Indices
    Signed-off-by: Nominal9977 <84743651+Nominal9977@users.noreply.github.com>

--- assignments/assignment2_jumpingDude/main.cpp
+++ assignments/assignment2_jumpingDude/main.cpp
@@ -14,11 +14,15 @@ const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
 float vertices[] = {
-    -0.5f,  -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
-     0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f
+     0.5f,  0.5f, 0.0f, 
+     0.5f, -0.5f, 0.0f,  
+    -0.5f, -0.5f, 0.0f, 
+    -0.5f,  0.5f, 0.0f   
 };
-
+unsigned int indices[] = {  
+    0, 1, 3,  
+    1, 2, 3  
+};
 using namespace std;
 
 int main() {
@@ -52,19 +56,24 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-
-
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
 
     shaderInfo.use();
 
+    // Googled thse two lines becouse i want to know the wire frame code!
+    //glPolygonMode(GL_FRONT, GL_LINE);
+    //glPolygonMode(GL_BACK, GL_LINE);
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+
 
         // Clear framebuffer
         glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
@@ -72,19 +81,15 @@ int main() {
 
         
         shaderInfo.use();
-        shaderInfo.setFloat("editColor", abs(sin(glfwGetTime())));  
-        shaderInfo.setFloat("editLocationy", glfwGetTime()); 
+        //shaderInfo.setFloat("editColor", abs(sin(glfwGetTime())));  
+        //shaderInfo.setFloat("editLocationy", glfwGetTime()); 
 
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+glBindVertexArray(0);
 
         glfwSwapBuffers(window);
     }
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    printf("Shutting down...");
-
     glfwTerminate();
     return 0;
 }

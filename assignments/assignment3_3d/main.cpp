6b05e0838f4b7ecf81f4039398cef028d3a73c37
Nominal9977 <84743651+Nominal9977@users.noreply.github.com>
2024-10-18 21:28:40 -0400

    kinda working but not really

--- assignments/assignment3_3d/main.cpp
+++ assignments/assignment3_3d/main.cpp
@@ -6,6 +6,8 @@
 #include <ew/ewMath/ewMath.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "ew/external/stb_image.h"
 #include "../core/ColorsnShit/ColorsnShit.h" 
 #include "../core/ColorsnShit/TextuerinShit.h"
@@ -16,11 +18,47 @@ const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
 float vertices[] = {
-        
-     0.25f,  0.25f, 0.0f,   1.0f, 0.4f, 0.9f,   1.0f, 1.0f,
-     0.25f, -0.25f, 0.0f,   1.0f, 0.4f, 0.9f,   1.0f, 0.0f,
-    -0.25f, -0.25f, 0.0f,   1.0f, 0.4f, 0.9f,   0.0f, 0.0f,
-    -0.25f,  0.25f, 0.0f,   1.0f, 0.4f, 0.9f,   0.0f, 1.0f
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 unsigned int indices[] = {
     0, 1, 3, 
@@ -28,6 +66,7 @@ unsigned int indices[] = {
 };
 
 using namespace std;
+using namespace glm;
 
 int main() {
     if (!glfwInit()) {
@@ -85,10 +124,10 @@ int main() {
     glGenTextures(1, &texture1);
 
     textuer.bind(texture1);
-    textuer.loadTextureImage("assets/cat.png", true, GL_LINEAR, GL_REPEAT);
+    textuer.loadTextureImage("assets/wall.jpg", true, GL_LINEAR, GL_REPEAT);
 
 
-    unsigned int bgTexture1, bgTexture2;
+   /* unsigned int bgTexture1, bgTexture2;
 
     glGenTextures(1, &bgTexture1);
     glGenTextures(1, &bgTexture2);
@@ -97,7 +136,7 @@ int main() {
     textuer.loadTextureImage("assets/wall.jpg", true, GL_LINEAR, GL_REPEAT);
 
     textuer.bind(bgTexture2);
-    textuer.loadTextureImage("assets/catfood.png", true, GL_LINEAR, GL_REPEAT);
+    textuer.loadTextureImage("assets/catfood.png", true, GL_LINEAR, GL_REPEAT);*/
 
 
 
@@ -107,6 +146,14 @@ int main() {
     backgroundShader.use();
     backgroundShader.setInt("bgTexture1", 0);
     backgroundShader.setInt("bgTexture2", 1);
+
+    unsigned int vectModel = glad_glGetUniformLocation(shaderInfo.ID, "model");
+    unsigned int vectView = glad_glGetUniformLocation(shaderInfo.ID, "view");
+    unsigned int vectPro = glad_glGetUniformLocation(shaderInfo.ID, "projection");
+
+
+    
+
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -123,9 +170,9 @@ int main() {
 
         glBindVertexArray(VAO);
 
-        backgroundShader.use();
+        //backgroundShader.use();
        
-        glUniform1f(glGetUniformLocation(backgroundShader.ID, "time"), timeValue);
+      /*  glUniform1f(glGetUniformLocation(backgroundShader.ID, "time"), timeValue);
         glUniform1f(glGetUniformLocation(backgroundShader.ID, "waveFrequency"), waveFrequency);
         glUniform1f(glGetUniformLocation(backgroundShader.ID, "waveAmplitude"), waveAmplitude);
 
@@ -133,15 +180,29 @@ int main() {
         glBindTexture(GL_TEXTURE_2D, bgTexture1);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, bgTexture2);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);*/
+
+        mat4 model = mat4(1.0f);
+        model = rotate(model, (float)glfwGetTime() *  radians(50.0f),  vec3(0.5f, 1.0f, 0.0f));
+         mat4 view =  mat4(1.0f);
+        view =  translate(view,  vec3(0.0f, 0.0f, -3.0f));
+        mat4 projection;
+        projection = perspective( radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 
         shaderInfo.use();
 
+
+        
+
         glUniform1f(glGetUniformLocation(shaderInfo.ID, "time"), timeValue);
+
+        glUniformMatrix4fv( vectModel, 1, GL_FALSE,   value_ptr(model));
+        glUniformMatrix4fv( vectView,  1,  GL_FALSE,  value_ptr(view));
+        glUniformMatrix4fv( vectPro,   1,   GL_FALSE, value_ptr(projection));
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
         glfwSwapBuffers(window);
